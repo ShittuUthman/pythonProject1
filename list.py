3a30d80e59f4aa074120a230d426030267dadcc9
ShittuUthman <88115601+ShittuUthman@users.noreply.github.com>
2021-08-02 22:33:34 -0700

    Delete list.py

--- list.py
+++
@@ -1,8 +0,0 @@
-import csv
-load_file = open("topCPU.txt.py", "r")
-raw = load_file.readlines()
-with open("topCPU.txt.py") as load_file:
-    reader = csv.reader(load_file, delimiter=" ")
-    data = [tuple(row) for row in reader]
-load_file.close()
-print (data)
